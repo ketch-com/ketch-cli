255ab7356d27ca9fe69a192901d7b44cc3a28212
Anirudhan J Rajagopalan <1160815+algorithmcardboard@users.noreply.github.com>
2022-09-06 13:23:27 -0700

    chore(paths): Use paths from the provided url (#123)

--- pkg/transponder/configure.go
+++ pkg/transponder/configure.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -29,7 +30,7 @@ func Configure(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	u.Path = fmt.Sprintf("/captain/connections/%s", args[0])
+	u.Path = path.Join(u.Path, "/captain/connections", args[0])
 
 	client := http.Client{}
 
